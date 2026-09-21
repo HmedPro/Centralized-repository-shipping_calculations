bc21ba7732d7825a64cb119ad1c1d06fd63fb5e2
Mohamed <lawalimanzo99@gmail.com>
2026-09-21 06:40:51 -0400

    Revert "ajout d'unee ligne supplementaire a Shipping_Cost_Calcullator.py"
    This reverts commit 5d85639c51422bd95b938805dbb26af60fd48688.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
-# Here is another update by HmedPro
-# Here is a new update by HmedPro
+# Hzere is a new update by HmedPro
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
